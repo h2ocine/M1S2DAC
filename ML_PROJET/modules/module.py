edb1a0aaf57460123391504d702c2d537f8d4abf
h2ocine <hocinekadem@hotmail.com>
2024-05-25 19:26:27 +0200

    .

--- ML_PROJET/modules/module.py
+++ ML_PROJET/modules/module.py
@@ -1,70 +1,85 @@
 import numpy as np
 
-class Module:
+class Module(object):
+
     def __init__(self):
         self._parameters = None
-        self._gradient = None
-        self._X = None
+        self._gradient   = None
+        self._X          = None
+
 
     def zero_grad(self):
-        """
-        Réinitialise le gradient à zéro.
-        """
-        if self._parameters is not None:
-            self._gradient = {k: np.zeros_like(v) for k, v in self._parameters.items()}
+        '''
+        set the gradient to 0
+        '''
+        pass
+
 
     def forward(self, X: np.ndarray) -> np.ndarray:
-        """
-        Effectue une passe avant (calcul des sorties).
-        :param X: données d'entrée, forme (m, d)
-        """
-        raise NotImplementedError("La méthode forward doit être implémentée par les sous-classes.")
+        '''
+        Compute the forward pass
+        :param X: input data
+            shape (m, d)
+        '''
+        raise NotImplementedError
+
 
     def _save_data(self, X: np.ndarray):
-        """
-        Sauvegarde les données d'entrée pour une utilisation lors de la rétropropagation.
-        :param X: données d'entrée, forme (m, d)
-        """
+        '''
+        Save forward data
+        :param X: input data
+            shape (m, d)
+        '''
         self._X = X
+
 
     def __call__(self, X: np.ndarray):
         return self.forward(X)
 
-    def update_parameters(self, gradient_step: float = 1e-3):
-        """
-        Met à jour les paramètres en utilisant le gradient calculé.
-        :param gradient_step: taux d'apprentissage
-        """
-        if self._parameters is not None and self._gradient is not None:
-            for k in self._parameters.keys():
-                self._parameters[k] -= gradient_step * self._gradient[k]
+
+    def update_parameters(self, gradient_step: float=1e-3):
+        '''
+        Calculation of the update of the parameters according to the calculated gradient 
+        and the step of gradient_step
+        :param gradient_step : learning rate
+        '''
+        pass
+
 
     def set_parameters(self, parameters):
-        """
-        Définit les paramètres du module.
-        :param parameters: dictionnaire de paramètres
-        """
-        self._parameters = parameters
+        '''
+        Set module parameters
+        :param parameters
+        '''
+        pass
 
+    
     def get_parameters(self):
-        """
-        Retourne les paramètres du module.
-        """
-        return self._parameters
+        '''
+        Get module parameters
+        '''
+        return None
+
 
     def backward_update_gradient(self, X: np.ndarray, delta: np.ndarray):
-        """
-        Met à jour le gradient des paramètres.
-        :param X: données d'entrée, forme (m, d)
-        :param delta: delta de la couche courante, forme (m, d')
-        """
-        raise NotImplementedError("La méthode backward_update_gradient doit être implémentée par les sous-classes.")
+        '''
+        Update the gradient value
+        :param X: input data
+            shape (m, d)
+        :param delta: current layer delta
+            shape (m, d')
+        '''
+        pass
+
 
     def backward_delta(self, X: np.ndarray, delta: np.ndarray) -> np.ndarray:
-        """
-        Calcule le delta pour la couche précédente.
-        :param X: données d'entrée, forme (m, d)
-        :param delta: delta de la couche courante, forme (m, d')
-        :return: delta pour la couche précédente, forme (m, d)
-        """
-        raise NotImplementedError("La méthode backward_delta doit être implémentée par les sous-classes.")
+        '''
+        Calculate the derivative of the error
+        :param X: input data
+            shape (m, d)
+        :param delta: current layer delta
+            shape (m, d')
+        :return previous layer delta
+            shape (m, d)
+        '''
+        raise NotImplementedError
